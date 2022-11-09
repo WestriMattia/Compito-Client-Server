2d84fec27e4088c85fab991d9d244ffff766c3f9
lai2-01 <lai2-01@itismeucci.com>
2022-11-09 09:10:49 +0100

    Fix, no bonus

--- wesserver/src/main/java/server/App.java
+++ wesserver/src/main/java/server/App.java
@@ -7,14 +7,13 @@ public class App{
   public static void main(String[] args) throws Exception {
     ServerSocket ss = new ServerSocket(3000);
     System.out.println("In ascolto sulla porta 3000");
-    final String nome="serverWestri";
     int c = 1;
     int b = 3;
     boolean running = true;
     while (running) {
       Socket s = ss.accept();
       System.out.println("Client n."+c+" connesso");
-      Handler client = new Handler(s, c, b, nome);
+      Handler client = new Handler(s, b);
       c++;
       client.start();
     }
